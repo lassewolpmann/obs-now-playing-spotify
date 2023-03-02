d99739773480f1aa52535d41434b18014cab0a3a
Lasse Wolpmann <lassewolpmann@outlook.de>
2023-03-02 19:26:57 +0200

    style fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,12 +35,6 @@
         clearInterval(playbackStateInterval);
     })
 </script>
-<svelte:head>
-    <link rel="stylesheet" href="/style.css?v=1.2">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
-</svelte:head>
 <canvas id="canvas" bind:this={albumCanvasEl}></canvas>
 {#if accessToken}
     {#if playbackData}
